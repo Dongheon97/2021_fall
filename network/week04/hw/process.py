9a5ba03bacdcad78400afaf3ab0774344ac8d3bc
Dongheon97 <jdd04063@gmail.com>
2021-10-05 20:40:11 +0900

    networks multiprocess/thread

--- network/week04/hw/process.py
+++ network/week04/hw/process.py
@@ -1,20 +1,31 @@
+from multiprocessing import Process
 import socket
 import time
+
+def multiprocess_sr(csocket, addr):
+    data = csocket.recv(1024)
+    print(f"[Client {addr} Info] {data.decode()}")
+    res = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
+    csocket.send(res.encode('utf-8'))
+    csocket.send(data)
+    csocket.close()
+    
 
 def main(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('', port))
     server_socket.listen()
+    req_clients = list()
 
-    while True:
-        (csocket, address) = server_socket.accept()
-        data = csocket.recv(1024)
-        print(f"[client {address} Info] {data.decode()}")
-        res = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
-        csocket.send(res.encode('utf-8'))
-        csocket.send(data)
-        csocket.close()
+    try:
+        csocket, addr = server_socket.accept()
+        proc = Process(target=multiprocess_sr, args=(csocket, addr,))
+        req_clients.append(proc)
+        proc.start()
+
+    except:
+        proc.join()
 
 if __name__ == '__main__':
-    port = 8889
+    port = 8890
     main(port)
--- network/week04/hw/thread.py
+++ network/week04/hw/thread.py
@@ -1,20 +1,31 @@
+from threading import Thread
 import socket
 import time
+
+def multithread_sr(csocket, addr):
+    data = csocket.recv(1024)
+    print(f"[Client {addr} Info] {data.decode()}")
+    res = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
+    csocket.send(res.encode('utf-8'))
+    csocket.send(data)
+    csocket.close()
+        
 
 def main(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('', port))
     server_socket.listen()
+    req_clients = list()
 
-    while True:
-        (csocket, address) = server_socket.accept()
-        data = csocket.recv(1024)
-        print(f"[client {address} Info] {data.decode()}")
-        res = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"
-        csocket.send(res.encode('utf-8'))
-        csocket.send(data)
-        csocket.close()
+    try:
+        csocket, addr = server_socket.accept()
+        th = Thread(target=multithread_sr, args=(csocket, addr,))
+        req_clients.append(th)
+        th.start()
+
+    except:
+        th.join()
 
 if __name__ == '__main__':
-    port = 8889
+    port = 8891
     main(port)
