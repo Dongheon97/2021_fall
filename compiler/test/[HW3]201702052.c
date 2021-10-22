ec7bda9710fe468d0efd696193598c8858f78168
Dongheon97 <jdd04063@gmail.com>
2021-10-22 22:17:16 +0900

    doing compiler hw

---
+++ compiler/test/[HW3]201702052.c
@@ -0,0 +1,10 @@
+int a;
+int b = 2;
+int c[30];
+int main(int x, int y)
+{
+....int i;
+....int j = 0;
+....int k[10];
+}
+
--- compiler/test/test.c
+++ compiler/test/test.c
@@ -1,15 +1,32 @@
-int a = 0;
-
-int main(){
-    int x;
-    x = 0;
-
-        if(x > 5)
-            x = x * 2;
-
-        else
-            x = x + 1;
-
-
-    return 0;
+int a;
+int b=2;
+int c[30];
+int main(int x, int y){
+    int i;
+    int j = 0;
+    int k[10];
 }
+
+/*
+
+int iread(void);
+
+int isqrt(int a, int guess) { // tail recursive integer square root
+int x;
+if (guess == (x = (guess + a/guess)/2)) // guess via Newton’s method
+return guess;
+return isqrt(a, x); // tail recurse
+}
+int num;
+void main(void) {
+num = iread();
+iprint(isqrt(num, num/2));
+}
+
+int test1(void){
+}
+int test2(int x){
+}
+int test3(int x[]){}
+int main(){int x;x = 0;if(x > 5)x = x * 2;elsex = x + 1;return 0;}
+*/
